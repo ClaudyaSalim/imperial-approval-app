df7ab8059f15980cb032996683e24be171d90636
ClaudyaSalim <claudya.salim@gmail.com>
2024-05-02 17:01:54 +0700

    modif search bar

--- lib/theme/color_scheme.dart
+++ lib/theme/color_scheme.dart
@@ -8,6 +8,7 @@ ColorScheme colorScheme = ColorScheme.fromSeed(
   primaryContainer: Color.fromRGBO(235, 221, 192, 1), // krem
   secondary: Color.fromRGBO(79, 43, 31, 1),
   tertiary: Color.fromRGBO(75, 92, 107, 1),
+  tertiaryContainer: Color.fromRGBO(255, 255, 255, 1),
   // onSecondary: onSecondary, 
   error: Color.fromRGBO(213, 83, 102, 1), 
   // onError: onError, 
--- lib/theme/search_bar_theme.dart
+++ lib/theme/search_bar_theme.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:imperial_approval_app/theme/color_scheme.dart';
 
 SearchBarThemeData searchBarTheme = SearchBarThemeData(
-  backgroundColor: const MaterialStatePropertyAll(Colors.transparent),
-  elevation: const MaterialStatePropertyAll(0),
+  backgroundColor: MaterialStatePropertyAll(colorScheme.tertiaryContainer),
+  elevation: const MaterialStatePropertyAll(5.0),
   shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0))),
-  side: MaterialStatePropertyAll(BorderSide(color: colorScheme.primary, width: 2))
+  // side: MaterialStatePropertyAll(BorderSide(color: colorScheme.primary, width: 2))
 );
 
 SearchViewThemeData searchDropDownTheme = SearchViewThemeData(
   surfaceTintColor: colorScheme.primary,
   backgroundColor: colorScheme.onPrimary,
   dividerColor: Colors.transparent,
-  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0), side: BorderSide.none),
   constraints: const BoxConstraints(maxHeight: 160)
 );
